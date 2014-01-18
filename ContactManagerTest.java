7e662f8778c20dc5d8201bf7f02895af2de1e1a9
RightyHo <andrewho@Andrews-iMac.local>
2014-01-18 22:17:45 +0000

    testing class for contact manager...started test for addFutureMeeting method

---
+++ ContactManagerTest.java
@@ -0,0 +1,10 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class ContactManagerTest {
+	@Test
+	public void testsAddFutureMeeting(){
+		addNewContact("Jamie O'Regan","Best buddy");
+		
+	}
+}
