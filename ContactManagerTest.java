b3a7bd246844b445928a0f465f7c0edfec56a6c4
RightyHo <andrewho@Andrews-iMac.local>
2014-01-19 14:49:13 +0000

    built out the general structure of the program except the implementations of the Meeting and Contact interfaces.  Wrote constructor for MyContacts class and started test for addFutureMeeting method

--- ContactManagerTest.java
+++ ContactManagerTest.java
@@ -2,9 +2,18 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class ContactManagerTest {
+	private ContactManager diary;
+	private Set<Contacts> contactsGroup;
+	public ContactManagerTest(){
+		diary = new LinkedList();	//Change to ArrayContactManager if decide to implement ContactManager as an array
+		contactsGroup = new HashSet<Contacts>(); //Change to TreeSet<Contacts> if the order of the elements is important
+	}
+	
 	@Test
 	public void testsAddFutureMeeting(){
-		addNewContact("Jamie O'Regan","Best buddy");
+		diary.addNewContact("Jamie O'Regan","Best buddy");
+		diary.addNewContact("Travis Wallach","MC");
+		diary.addNewContact("Wade Kelly","Groomsman");
 		
 	}
 }
